057376aac68672ec314d229782b7c1d0b081fff4
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-06-20 16:08:54 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <div>
+                <div className="container bg-amber-300 mx-auto">
+                    <h1 className="text-6xl">Coding is easy</h1>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
