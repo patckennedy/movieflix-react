86447fd5d87dc6482676cb0f88569a21256f5584
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-06-22 16:18:12 -0500

    Add project assets and update App.jsx with initial hero layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-    return <div>{/* Movies App starting point */}</div>;
-}
+import React from 'react';
+import './index.css';
+
+const App = () => {
+    return (
+        <>
+            <main>
+                <div className="pattern" />
+                <div className="wrapper">
+                    <header>
+                        <img src='./hero.png' alt="Hero Banner" />
+                        <h1>
+                            Find <span className="text-gradient">Movies</span>{' '}
+                            You'll Enjoy Without the Hassle
+                        </h1>
+                    </header>
+                    <p>Search</p>
+                </div>
+            </main>
+        </>
+    );
+};
 
 export default App;
