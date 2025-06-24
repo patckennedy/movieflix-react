950e61b1ee1061f8bae9f1393c61e98f3daa164e
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-06-23 20:29:26 -0500

    Update search.svg file path

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import './index.css';
+import Search from './components/search';
+import { useState } from 'react';
 
+// Main App Component
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
     return (
         <>
             <main>
                 <div className="pattern" />
                 <div className="wrapper">
                     <header>
-                        <img src='./hero.png' alt="Hero Banner" />
-                        <h1>
+                        <img src="./hero.png" alt="Hero Banner" />
+                        {/* <h1>
                             Find <span className="text-gradient">Movies</span>{' '}
                             You'll Enjoy Without the Hassle
+                        </h1> */}
+                        <h1>
+                            Find <span className="text-gradient">Movies</span>
+                            You Will Enjoy With the Hassle
                         </h1>
                     </header>
-                    <p>Search</p>
+                    {/* Search component */}
+                    <Search
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                    />
                 </div>
             </main>
         </>
